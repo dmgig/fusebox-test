8a5902276d78391303033f8930f3a78b65668b73
Dave Giglio <dgiglio@penguinrandomhouse.com>
2018-02-08 11:05:54 -0500

    initial commit

---
+++ fuse.js
@@ -0,0 +1,31 @@
+const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin, Sparky } = require("fuse-box");
+
+const fuse = FuseBox.init({
+  homeDir: "src",
+  output: "dist/$name.js",
+  plugins: [
+    [SassPlugin(), CSSPlugin()],
+    CSSPlugin(),
+    WebIndexPlugin({path: "."})
+  ]
+});
+
+fuse.dev();
+
+fuse.bundle("app")
+  .instructions("> index.ts")
+  .watch()
+  .hmr();
+
+Sparky.task("clean", () => {
+  return Sparky.src("dist").clean("dist");
+});
+
+Sparky.task("watch:images", () => {
+  return Sparky.watch("**/*.+(svg|png|jpg|gif)", {base: "./src"})
+    .dest("./dist");
+});
+
+Sparky.task("default", ["clean", "watch:images"], () => {
+  fuse.run();
+});
